3b2bb49c2f5eee274a8f0f4baa5ef8139332b73d
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-18 21:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(Object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 4e9fbf9e8b89daf3a930938d383bbb2c62a47391
